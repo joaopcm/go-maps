8faf91d8c6fe9529f6784ecda3a9563dcf5bb474
João Melo <jopcmelo@gmail.com>
2021-11-25 09:18:52 -0300

    feat: iterating over maps

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red": "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// var colors map[string]string
+	printMap(colors)
+}
 
-	colors := make(map[string]string)
-
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
